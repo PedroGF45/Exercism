861144131f2d1f3d2d148343b3e29ecd500f6bb0
Pedro Brito <pedrobfh@gmail.com>
2023-02-15 17:36:57 +0000

    Removed TODO labels

--- python/guidos-gorgeous-lasagna/lasagna.py
+++ python/guidos-gorgeous-lasagna/lasagna.py
@@ -3,14 +3,9 @@
 Learn about Guido, the creator of the Python language: https://en.wikipedia.org/wiki/Guido_van_Rossum
 """
 
-# TODO: define the 'EXPECTED_BAKE_TIME' constant
-# TODO: consider defining the 'PREPARATION_TIME' constant
-#       equal to the time it takes to prepare a single layer
-
 EXPECTED_BAKE_TIME = 40
 PREPARATION_TIME = 2
 
-# TODO: define the 'bake_time_remaining()' function
 def bake_time_remaining(elapsed_bake_time):
     """Calculate the bake time remaining.
     
@@ -24,8 +19,6 @@ def bake_time_remaining(elapsed_bake_time):
     return EXPECTED_BAKE_TIME - elapsed_bake_time
 
 
-# TODO: define the 'preparation_time_in_minutes()' function
-#       and consider using 'PREPARATION_TIME' here
 def preparation_time_in_minutes(layers):
     '''
     :param layers: int - number of layers on the pizza
@@ -33,7 +26,6 @@ def preparation_time_in_minutes(layers):
     '''
     return layers * PREPARATION_TIME
 
-# TODO: define the 'elapsed_time_in_minutes()' function
 def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
     '''
     :param number_of_layers: int - number of layers used on the pizza
