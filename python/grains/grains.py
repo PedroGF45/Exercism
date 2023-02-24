e5f0694ea6e53dece804e1f2ddeceda3a80b56fe
Pedro Brito <pedrobfh@gmail.com>
2023-02-24 17:41:02 +0000

    Solved exercise 24

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,13 @@
 def square(number):
-    pass
-
+    if (number <= 64 and number > 0):
+        return 2**(number -1)
+    raise ValueError("square must be between 1 and 64")
 
 def total():
-    pass
+    squares = 64
+    i = 0
+    res = 0
+    while i < squares:
+        res += 2**i
+        i += 1
+    return res
