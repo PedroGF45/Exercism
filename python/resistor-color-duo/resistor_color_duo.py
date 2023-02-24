3d903945de83abfd283224d6e7ef997e4843f722
Pedro Brito <pedrobfh@gmail.com>
2023-02-24 15:47:48 +0000

    Solved exercise 21

--- python/resistor-color-duo/resistor_color_duo.py
+++ python/resistor-color-duo/resistor_color_duo.py
@@ -1,2 +1,13 @@
 def value(colors):
-    pass
+    colors_inventory = ['black',
+                        'brown',
+                        'red',
+                        'orange',
+                        'yellow',
+                        'green',
+                        'blue',
+                        'violet',
+                        'grey',
+                        'white']
+    first_color, second_color, *rest = colors
+    return int(f'{colors_inventory.index(first_color)}{colors_inventory.index(second_color)}')
