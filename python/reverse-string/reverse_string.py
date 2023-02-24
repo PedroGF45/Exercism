7709155ecd1fb983495e91672ac70c83b6661dd2
Pedro Brito <pedrobfh@gmail.com>
2023-02-24 14:20:36 +0000

    Added other solutions

--- python/reverse-string/reverse_string.py
+++ python/reverse-string/reverse_string.py
@@ -5,3 +5,14 @@ def reverse(text):
         new_text += text[len(text) - 1 - i]
         i += 1
     return new_text
+
+def reverse_option(text):
+    a = list((map(lambda x: text[-x -1], range(len(text)))))
+    new_text = ''
+    for item in a:
+        new_text += item
+    return new_text
+
+def reverse_option_option(text):
+    return text[::-1]
+    
