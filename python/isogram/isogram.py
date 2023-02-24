b264ed5c5e7a6ff5f819b75b531408ac604a62b1
Pedro Brito <pedrobfh@gmail.com>
2023-02-24 16:13:17 +0000

    Bool not necessary

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,3 +1,3 @@
 def is_isogram(string):
     new_string = string.replace('-', '').replace(' ', '').lower()
-    return bool(len(set(new_string)) == len(new_string)) # set gives unique inputs
+    return len(set(new_string)) == len(new_string) # set gives unique inputs
