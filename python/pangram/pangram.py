4331f5b4a69b1a5020c5653bc26ece1f64ad2f64
Pedro Brito <pedrobfh@gmail.com>
2023-02-24 16:00:59 +0000

    Bool not necessary

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -3,4 +3,4 @@ import string
 alphabet = list(string.ascii_lowercase)
 
 def is_pangram(sentence):
-    return bool(all(map(lambda x: x in sentence.lower(), alphabet)))
+    return all(map(lambda x: x in sentence.lower(), alphabet))
