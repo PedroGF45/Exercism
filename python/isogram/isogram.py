b1578e9e639231069336941826afb80eb41b7098
Pedro Brito <pedrobfh@gmail.com>
2023-02-24 16:11:45 +0000

    Solved exercise 23

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,3 @@
 def is_isogram(string):
-    pass
+    new_string = string.replace('-', '').replace(' ', '').lower()
+    return bool(len(set(new_string)) == len(new_string)) # set gives unique inputs
