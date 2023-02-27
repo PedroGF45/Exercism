7d97e43f39b159e862ede9ccda0950e6c87b4820
Pedro Brito <pedrobfh@gmail.com>
2023-02-27 22:51:43 +0000

    Solved exercise 25

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,10 @@
 def distance(strand_a, strand_b):
-    pass
+    if (len(strand_a) != len(strand_b)):
+        raise ValueError("Strands must be of equal length.") # raise error due to unmatched strand lengths
+    
+    hamming_distance = 0
+    for i in range(len(strand_a)):
+        if strand_a[i] != strand_b[i]:
+            hamming_distance += 1
+            
+    return hamming_distance
