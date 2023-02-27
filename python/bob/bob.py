7791c7f92d9304df58335820c1315695a19ec0be
Pedro Brito <pedrobfh@gmail.com>
2023-02-27 23:11:58 +0000

    Solved exercise 26

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,2 +1,12 @@
 def response(hey_bob):
-    pass
+    hey_bob_cleaned = hey_bob.strip()
+    if hey_bob_cleaned.isupper() and hey_bob_cleaned[-1] == '?':
+        return 'Calm down, I know what I\'m doing!'
+    elif hey_bob_cleaned == "":
+        return 'Fine. Be that way!'
+    elif hey_bob_cleaned.isupper():
+        return 'Whoa, chill out!'
+    elif hey_bob_cleaned[-1] == '?':
+        return 'Sure.'
+    else:
+        return 'Whatever.'
