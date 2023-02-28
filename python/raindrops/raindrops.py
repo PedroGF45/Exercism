05a470ff3e234dcac7630575f74ddf45081c4f52
Pedro Brito <pedrobfh@gmail.com>
2023-02-28 22:58:41 +0000

    Solved exercise 31

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,19 @@
 def convert(number):
-    pass
+    
+    # check if number isn't a factor for any of the numbers
+    if (number % 3 != 0 and number % 5 != 0 and number % 7 != 0):
+        return str(number)
+
+    raindrops = {
+        '3': 'Pling',
+        '5': 'Plang',
+        '7': 'Plong'
+    }
+
+    res = ''
+
+    for factor in raindrops:
+        if number % int(factor) == 0:
+            res += raindrops[factor]
+    
+    return res
