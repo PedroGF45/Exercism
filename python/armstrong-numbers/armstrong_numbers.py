67a621b992245e15f33eb89e8fd1c68edd9fd449
Pedro Brito <pedrobfh@gmail.com>
2023-02-28 19:48:21 +0000

    Solved exercise 28

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,10 @@
 def is_armstrong_number(number):
-    pass
+
+    # converts number to a string
+    string_number = str(number)
+
+    # sum each digit each raised to the power of the total number of digits
+    sum_digits = sum(map(lambda x: int(x)**len(string_number), string_number))
+
+    # outputs true if is armstrong number
+    return bool(sum_digits == number)
