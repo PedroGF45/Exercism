9330982d226d7103d791c29ffb0834bb8f6ac073
Pedro Brito <pedrobfh@gmail.com>
2023-02-28 22:10:22 +0000

    Solved exercise 29

--- python/etl/etl.py
+++ python/etl/etl.py
@@ -1,2 +1,11 @@
 def transform(legacy_data):
-    pass
+
+    # initialize a new dictionary to store the new data system
+    new_data = {}
+
+    for score in legacy_data:
+        # for each letter in each score
+        for letter in legacy_data[score]:
+            new_data[letter.lower()] = score
+
+    return new_data
