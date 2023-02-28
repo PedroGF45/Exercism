7e2409108c896197b189fcbb99c4df27713bbe36
Pedro Brito <pedrobfh@gmail.com>
2023-02-28 19:16:00 +0000

    Solved exercise 27

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,2 +1,12 @@
 def to_rna(dna_strand):
-    pass
+    rna_strand = ''
+    for item in dna_strand:
+        if item == 'G':
+            rna_strand += 'C'
+        elif item == 'C':
+            rna_strand += 'G'
+        elif item == 'T':
+            rna_strand += 'A'
+        else:
+            rna_strand += 'U'
+    return rna_strand
