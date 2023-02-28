65ca6a77559b59764407d5a456f6f515f0dc142d
Pedro Brito <pedrobfh@gmail.com>
2023-02-28 22:39:41 +0000

    Solved exercise 30

--- python/darts/darts.py
+++ python/darts/darts.py
@@ -1,2 +1,12 @@
+from math import sqrt
+
 def score(x, y):
-    pass
+    if sqrt(x**2 + y**2) <= 1:
+        return 10
+    elif sqrt(x**2 + y**2) <= 5:
+        return 5
+    elif sqrt(x**2 + y**2) <= 10:
+        return 1
+    else: 
+        return 0
+    
