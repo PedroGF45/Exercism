84c7e0965bd93dbd8e9adc76afe4a278e74dc487
Pedro Brito <pedrobfh@gmail.com>
2023-02-28 22:42:35 +0000

    Refactored

--- python/darts/darts.py
+++ python/darts/darts.py
@@ -3,11 +3,13 @@ from math import sqrt
 def score(x, y):
 
     # using Pythagorean theorem to determine the value of the hypotenuse
-    if sqrt(x**2 + y**2) <= 1:
+    hypotenuse = sqrt(x**2 + y**2)
+    
+    if hypotenuse <= 1:
         return 10
-    elif sqrt(x**2 + y**2) <= 5:
+    elif hypotenuse <= 5:
         return 5
-    elif sqrt(x**2 + y**2) <= 10:
+    elif hypotenuse <= 10:
         return 1
     else: 
         return 0
