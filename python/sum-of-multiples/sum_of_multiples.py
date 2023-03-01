f684c6c59c078dd5676545ac12ecb57eb1e7a95b
Pedro Brito <pedrobfh@gmail.com>
2023-03-01 17:03:37 +0000

    Solved exercise 31

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,9 @@
 def sum_of_multiples(limit, multiples):
-    pass
+    natural = set()
+    for multiple in multiples: 
+        if multiple == 0:
+            continue
+        for number in range(1, limit):
+            if number % multiple == 0:
+                natural.update([number])
+    return sum(natural)
