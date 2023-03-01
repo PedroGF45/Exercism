b13c02be4f74ed9473b0bd08f362250df70697f1
Pedro Brito <pedrobfh@gmail.com>
2023-03-01 21:51:52 +0000

    Added sort method

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,2 +1,8 @@
 def find_anagrams(word, candidates):
-    pass
+    res = []
+    for candidate in candidates:
+        # the sorted strings are checked
+        if(sorted(candidate) == sorted(word)):
+            res += [candidate]
+
+    return res
