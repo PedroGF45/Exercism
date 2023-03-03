b66f8fd7630efde8ea0bba87adb8236d60f1c7c8
Pedro Brito <pedrobfh@gmail.com>
2023-03-03 13:59:42 +0000

    Solved exercise 33

--- python/difference-of-squares/difference_of_squares.py
+++ python/difference-of-squares/difference_of_squares.py
@@ -1,10 +1,20 @@
 def square_of_sum(number):
-    pass
+    i = 1
+    sum = 0
+    while i <= number:
+        sum += i
+        i += 1
+    return sum**2
 
 
 def sum_of_squares(number):
-    pass
+    i = 1
+    sum = 0
+    while i <= number:
+        sum += i**2
+        i += 1
+    return sum
 
 
 def difference_of_squares(number):
-    pass
+    return square_of_sum(number) - sum_of_squares(number)
