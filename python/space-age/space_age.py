264940e3bf0c892ca3e32f570df6d14befe7c0c4
Pedro Brito <pedrobfh@gmail.com>
2023-03-03 20:35:59 +0000

    Better constant

--- python/space-age/space_age.py
+++ python/space-age/space_age.py
@@ -1,14 +1,16 @@
 class SpaceAge:
+
+    EARTH_SECONDS_YEAR = 31557600
+
     def __init__(self, seconds):
-        EARTH_SECONDS_YEAR = 31557600
-        self.mercury = seconds / EARTH_SECONDS_YEAR / 0.2408467
-        self.venus = seconds / EARTH_SECONDS_YEAR / 0.61519726
-        self.earth = seconds / EARTH_SECONDS_YEAR
-        self.mars = seconds / EARTH_SECONDS_YEAR / 1.8808158
-        self.jupiter = seconds / EARTH_SECONDS_YEAR / 11.862615
-        self.saturn = seconds / EARTH_SECONDS_YEAR / 29.447498
-        self.uranus = seconds / EARTH_SECONDS_YEAR / 84.016846
-        self.neptune = seconds / EARTH_SECONDS_YEAR / 164.79132
+        self.mercury = seconds / self.EARTH_SECONDS_YEAR / 0.2408467
+        self.venus = seconds / self.EARTH_SECONDS_YEAR / 0.61519726
+        self.earth = seconds / self.EARTH_SECONDS_YEAR
+        self.mars = seconds / self.EARTH_SECONDS_YEAR / 1.8808158
+        self.jupiter = seconds / self.EARTH_SECONDS_YEAR / 11.862615
+        self.saturn = seconds / self.EARTH_SECONDS_YEAR / 29.447498
+        self.uranus = seconds / self.EARTH_SECONDS_YEAR / 84.016846
+        self.neptune = seconds / self.EARTH_SECONDS_YEAR / 164.79132
     
     def on_mercury(self):
         return float(f'{self.mercury:.2f}')
