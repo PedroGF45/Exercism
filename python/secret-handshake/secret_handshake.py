7d07aebca46324190962a953d98217900952be62
Pedro Brito <pedrobfh@gmail.com>
2023-03-03 21:12:44 +0000

    Solved exercise 40

--- python/secret-handshake/secret_handshake.py
+++ python/secret-handshake/secret_handshake.py
@@ -1,2 +1,23 @@
 def commands(binary_str):
-    pass
+
+    # actions with their binnary indeces
+    actions = {
+        '4': 'wink',
+        '3': 'double blink',
+        '2': 'close your eyes',
+        '1': 'jump'
+    }
+
+    # initialize handshake list
+    handshake = []
+
+    for bin, act in actions.items():
+        if binary_str[int(bin)] == '1':
+            handshake += [act]
+
+    # if first element is 1 then reverse the actions in handshake
+    if binary_str[0] == '1':
+        handshake.reverse()
+        return handshake
+    else:
+        return handshake
