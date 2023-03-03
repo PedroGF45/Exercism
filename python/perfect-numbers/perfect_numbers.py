54cf69955dd49981448aa1839b012a1cba16c49e
Pedro Brito <pedrobfh@gmail.com>
2023-03-03 17:57:13 +0000

    Solved exercise 35, Added 36 template

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -4,4 +4,20 @@ def classify(number):
     :param number: int a positive integer
     :return: str the classification of the input integer
     """
-    pass
+    # define aliquot sum
+    aliquot_sum = 0
+
+    # if a number to be classified is less than 1.
+    if (number < 1):
+        raise ValueError("Classification is only possible for positive integers.")
+    else:
+        for i in range(1, number):
+            if number % i == 0:
+                aliquot_sum += i
+    
+    if aliquot_sum == number:
+        return 'perfect'
+    elif aliquot_sum > number:
+        return 'abundant'
+    else:
+        return 'deficient'
