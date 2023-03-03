a881409b38bafee51774abcb994ccfd246fe9232
Pedro Brito <pedrobfh@gmail.com>
2023-03-03 19:35:51 +0000

    Solved exercise 37

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,2 +1,27 @@
 def is_valid(isbn):
-    pass
+    # Remove hifens and transfrom to a list
+    isbn_clean = isbn.replace('-', '')
+    
+    # Check if all elements except last is int
+    if not isbn_clean[:-1].isdigit():
+        return False
+
+    # if not 10 character, not valid
+    if len(isbn_clean) != 10:
+        return False
+
+    isbn_clean = list(isbn_clean)
+
+    # Check if last digit is 'X' or digit
+    if isbn_clean[-1] == 'X':
+        isbn_clean[-1] = '10'
+    elif not isbn_clean[-1].isdigit():
+        return False
+    
+    # Do the calculations
+    sum_val = 0
+    for i in range(10, 0, -1):
+        sum_val += i * int(isbn_clean[10-i])
+    
+    # Check valdiation
+    return sum_val % 11 == 0
