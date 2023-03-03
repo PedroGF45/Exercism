2ce1aa18891b5d21e8dc0632e1cbeb0c549b647e
Pedro Brito <pedrobfh@gmail.com>
2023-03-03 14:44:36 +0000

    Solved exercise 34

--- python/flatten-array/flatten_array.py
+++ python/flatten-array/flatten_array.py
@@ -1,2 +1,10 @@
 def flatten(iterable):
-    pass
+    if iterable is None or len(iterable) == 0 :
+        return []
+    for i in range(len(iterable)):
+        if isinstance(iterable[i], int):
+            return [iterable[i]] + flatten(iterable[i+1:])
+        else:
+            return flatten(iterable[i]) + flatten(iterable[i+1:])
+        
+        
