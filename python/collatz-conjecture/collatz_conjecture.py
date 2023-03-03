5e0aa91dd8aeed0484d081d1705a4f144c370e6c
Pedro Brito <pedrobfh@gmail.com>
2023-03-03 20:47:51 +0000

    Solved exercise 39

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,19 @@
 def steps(number):
-    pass
+    
+    # if number is 0 or negative return error
+    if number <= 0:
+        raise ValueError('Only positive integers are allowed')
+    
+    steps = 0
+    while number != 1:
+
+        #if number is even divide by 2
+        if number % 2 == 0:
+            number /= 2
+        # if number is odd
+        else:
+            number =  number * 3 + 1
+        
+        steps += 1
+    
+    return steps
