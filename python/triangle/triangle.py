033fdffe37073424b6817455e2345c0c4fc6f418
Pedro Brito <pedrobfh@gmail.com>
2023-03-07 20:43:18 +0000

    Solved exercise 43

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,10 +1,41 @@
 def equilateral(sides):
-    pass
+    # side can't have 0 length
+    if any(side == 0 for side in sides):
+        return False
+    
+    # triangle sum of sides violation
+    if sides[0] + sides[1] < sides[2] or sides[1] + sides[2] < sides[0] or sides[0] + sides[2] < sides[1]:
+        return False
 
+    # if all sides are equal
+    if sides[0] == sides[1] and sides[1] == sides[2]:
+        return True
+    return False
 
 def isosceles(sides):
-    pass
-
+    # side can't have 0 length
+    if any(side == 0 for side in sides):
+        return False
+    
+    # triangle sum of sides violation
+    if sides[0] + sides[1] < sides[2] or sides[1] + sides[2] < sides[0] or sides[0] + sides[2] < sides[1]:
+        return False
+    
+    # At least 2 sides are equal
+    if sides[0] == sides[1] or sides[0] == sides[2] or sides[1] == sides[2]:
+        return True
+    return False 
 
 def scalene(sides):
-    pass
+    # side can't have 0 length
+    if any(side == 0 for side in sides):
+        return False
+    
+    # triangle sum of sides violation
+    if sides[0] + sides[1] < sides[2] or sides[1] + sides[2] < sides[0] or sides[0] + sides[2] < sides[1]:
+        return False
+    
+    # At least 2 sides are equal
+    if sides[0] != sides[1] and sides[0] != sides[2] and sides[1] != sides[2]:
+        return True
+    return False 
