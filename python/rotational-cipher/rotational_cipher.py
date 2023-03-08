8f5592ca224ebc379d290b555a72dcde2f4324ba
Pedro Brito <pedrobfh@gmail.com>
2023-03-08 23:48:30 +0000

    Solved exercise 45

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -1,2 +1,23 @@
 def rotate(text, key):
-    pass
+
+    # Define letters
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+
+    def check_index(index, key):
+        if index + key < 26:
+            return index + key
+        else:
+            return index + key - 26
+
+    ans = ''
+
+    for text_letter in text:
+        print(text_letter)
+        if text_letter.isalpha() and text_letter.isupper():
+            ans += letters[check_index(letters.rindex(text_letter.lower()), key)].upper()
+        elif text_letter.isalpha() and text_letter.islower():
+            ans += letters[check_index(letters.rindex(text_letter), key)]
+        else:
+            ans += text_letter
+
+    return ans
