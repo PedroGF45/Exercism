d844b4f845ac06ca258759d17eb316e06a05a374
Pedro Brito <pedrobfh@gmail.com>
2023-03-09 21:56:23 +0000

    Solved exercise 54

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -1,6 +1,25 @@
-def encode(plain_text):
-    pass
+import string
 
+LETTERS = ['abcdefghijklmnopqrstuvwxyz', 'zyxwvutsrqponmlkjihgfedcba']
+
+def encode(plain_text):
+    plain_text = plain_text.translate(str.maketrans('', '', string.punctuation)).replace(' ', '')
+    ans = ''
+    for letter in plain_text:
+        if letter.isdigit():
+            ans += letter
+        elif len(ans) == 5 or (len(ans) in range(5, len(plain_text) + 1, 6)) :
+            ans += ' ' + LETTERS[1][LETTERS[0].index(letter.lower())]
+        else:
+            ans += LETTERS[1][LETTERS[0].index(letter.lower())]
+    return ans
 
 def decode(ciphered_text):
-    pass
+    ciphered_text = ciphered_text.translate(str.maketrans('', '', string.punctuation)).replace(' ', '')
+    ans = ''
+    for letter in ciphered_text:
+        if letter.isdigit():
+            ans += letter
+        else:
+            ans += LETTERS[0][LETTERS[1].index(letter.lower())] 
+    return ans
