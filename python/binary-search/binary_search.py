14af50ab21841eb70b6b9057749e9216389b31f3
Pedro Brito <pedrobfh@gmail.com>
2023-03-09 13:17:44 +0000

    solved exercise 46

--- python/binary-search/binary_search.py
+++ python/binary-search/binary_search.py
@@ -1,2 +1,18 @@
-def find(search_list, value):
-    pass
+def find(list, value):
+
+    copy_search = list.copy()
+
+    # sorting algorithm
+    def aux(copy_search, value):
+        if value == copy_search[len(copy_search) // 2]:
+            return list.index(value)
+        elif value > copy_search[len(copy_search) // 2]:
+            return aux(copy_search[len(copy_search) // 2 + 1:], value)
+        else: 
+            return aux(copy_search[:len(copy_search) // 2], value)
+    
+    # value is not in list
+    if value not in copy_search:
+        raise ValueError('value not in array')
+    else:
+        return aux(copy_search, value)
