6d98f49f0a8f4f2cbe17943450582a1162f364dd
Pedro Brito <pedrobfh@gmail.com>
2023-03-09 14:06:29 +0000

    Solved exercise 48

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,2 +1,5 @@
+import string
+
 def abbreviate(words):
-    pass
+    new_words = words.replace('-',' ').translate(str.maketrans('', '', string.punctuation)).split()
+    return ''.join(map(lambda x: x[0].upper(), new_words))
