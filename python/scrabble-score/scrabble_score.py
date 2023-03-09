85efcfd135bb7a5a1e7d2de9b9d017d310eb0e6a
Pedro Brito <pedrobfh@gmail.com>
2023-03-09 20:30:53 +0000

    Solved exercise 52

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,2 +1,20 @@
 def score(word):
-    pass
+
+    letter_value = {
+        'aeioulnrst': 1,
+        'dg': 2,
+        'bcmp': 3,
+        'fhvwy': 4,
+        'k': 5,
+        'jx': 8,
+        'qz': 10
+    }
+
+    score = 0
+
+    for letter_word in word:
+        for letter, value in letter_value.items():
+            if letter_word.lower() in letter:
+                score += value
+
+    return score
