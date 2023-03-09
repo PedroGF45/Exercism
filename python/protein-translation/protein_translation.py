c72bb0de5efa6a5ce549e97cc0de55b93fdeb46a
Pedro Brito <pedrobfh@gmail.com>
2023-03-09 16:38:54 +0000

    Solved exercise 50

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,2 +1,32 @@
 def proteins(strand):
-    pass
+    
+    codons = {
+        'AUG': 'Methionine',
+        ('UUU', 'UUC'): 'Phenylalanine',
+        ('UUA', 'UUG'): 'Leucine',
+        ('UCU', 'UCC', 'UCA', 'UCG'): 'Serine',
+        ('UAU', 'UAC'): 'Tyrosine',
+        ('UGU', 'UGC'): 'Cysteine',
+        'UGG': 'Tryptophan',
+        ('UAA', 'UAG', 'UGA'): 'STOP'
+    }
+
+    STOP_LIST = ['UAA', 'UAG', 'UGA']
+
+    codons_list = []
+    i = 0
+
+    # form a codon list
+    while i < len(strand):
+        codons_list += [strand[i:i+3]]
+        i += 3
+    translation = []
+
+    for codon_strand in codons_list:
+        for codon, protein in codons.items():
+            if codon_strand in STOP_LIST:
+                return translation
+            elif codon_strand in codon:
+                translation += [protein]
+
+    return translation
