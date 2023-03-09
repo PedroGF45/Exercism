bd9daaad767bf35e89b0ef419bb5f82b71bb956a
Pedro Brito <pedrobfh@gmail.com>
2023-03-09 22:30:11 +0000

    Solved exercise 54

--- python/resistor-color-trio/resistor_color_trio.py
+++ python/resistor-color-trio/resistor_color_trio.py
@@ -1,2 +1,35 @@
 def label(colors):
-    pass
+    colors_inventory = ['black',
+                        'brown',
+                        'red',
+                        'orange',
+                        'yellow',
+                        'green',
+                        'blue',
+                        'violet',
+                        'grey',
+                        'white']
+    first_color, second_color, third_color, *rest = colors
+
+    power_label = 10**colors_inventory.index(third_color)
+
+    resistance = int(f'{colors_inventory.index(first_color)}{colors_inventory.index(second_color)}') * power_label
+
+    prefix = ['kilo', 'mega', 'giga']
+
+    print(power_label)
+    print(resistance)
+
+    if resistance == 0:
+        return '0 ohms'
+    if int(resistance * 10 ** (-9)) == resistance * 10 ** (-9):
+        prefix = prefix[2]
+        return f'{int(resistance * 10 ** (-9))} {prefix}ohms'
+    elif int(resistance * 10 ** (-6)) == resistance * 10 ** (-6):
+        prefix = prefix[1]
+        return f'{int(resistance * 10 ** (-6))} {prefix}ohms'
+    elif int(resistance * 10 ** (-3)) == resistance * 10 ** (-3):
+        prefix = prefix[0]
+        return f'{int(resistance * 10 ** (-3))} {prefix}ohms'
+    else:
+        return f'{resistance} ohms'
