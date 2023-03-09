738ba299272b9f656351e54e0d1f5a29d7e3fddf
Pedro Brito <pedrobfh@gmail.com>
2023-03-09 13:19:22 +0000

    Refactored

--- python/binary-search/binary_search.py
+++ python/binary-search/binary_search.py
@@ -1,15 +1,16 @@
 def find(list, value):
 
     copy_search = list.copy()
+    index = len(copy_search) // 2
 
     # sorting algorithm
     def aux(copy_search, value):
-        if value == copy_search[len(copy_search) // 2]:
+        if value == copy_search[index]:
             return list.index(value)
-        elif value > copy_search[len(copy_search) // 2]:
-            return aux(copy_search[len(copy_search) // 2 + 1:], value)
+        elif value > copy_search[index]:
+            return aux(copy_search[index + 1:], value)
         else: 
-            return aux(copy_search[:len(copy_search) // 2], value)
+            return aux(copy_search[:index], value)
     
     # value is not in list
     if value not in copy_search:
