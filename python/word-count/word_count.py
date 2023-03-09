ac96a767ee42c8f29693f7ef2e955c83fb3b98b8
Pedro Brito <pedrobfh@gmail.com>
2023-03-09 23:16:43 +0000

    Solved exercise 55

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,2 +1,9 @@
+import string
+
 def count_words(sentence):
-    pass
+    split_words = sentence.strip("'").replace('\t', ' ').replace('\n', ' ').translate(str.maketrans(string.punctuation.replace("'", ''), ' '*len(string.punctuation.replace("'", '')))).lower().split()
+    cleaned_sentence = [word.strip("'") for word in split_words]
+    ans = {}
+    for word in set(cleaned_sentence):
+        ans[word] = cleaned_sentence.count(word)
+    return ans
