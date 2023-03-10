f4667687e66e7ed3a635501f1f524a22c811d584
Pedro Brito <pedrobfh@gmail.com>
2023-03-10 17:27:35 +0000

    Solved exercise 56

--- python/proverb/proverb.py
+++ python/proverb/proverb.py
@@ -1,2 +1,12 @@
-def proverb():
-    pass
+def proverb(*words, qualifier=None):
+    ans = []
+
+    for index, word in enumerate(words):
+        if index == (len(words) - 1) and qualifier is None:
+            ans += [f'And all for the want of a {words[0]}.'] 
+        elif index == (len(words) - 1) and qualifier is not None:
+            ans += [f'And all for the want of a {qualifier} {words[0]}.'] 
+        else:
+            ans += [f'For want of a {word} the {words[index + 1]} was lost.']
+    
+    return ans
