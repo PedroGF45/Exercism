036ea6f86e08f385e4e8f3857a0850e133c67f58
Pedro Brito <pedrobfh@gmail.com>
2023-03-16 15:51:02 +0000

    Solved exercise 58

--- python/robot-name/robot_name.py
+++ python/robot-name/robot_name.py
@@ -1,3 +1,25 @@
+import string
+import random
+
+alphabet = string.ascii_uppercase
 class Robot:
+
+    # all names being used
+    names = []
+
     def __init__(self):
-        pass
+        self.reset()
+
+    def reset(self):
+        
+        # construct the name of the robot
+        letters = ''.join(random.choice(alphabet) for i in range(2))
+        numbers = ''.join(str(random.randint(0,9)) for i in range(3))
+        name = letters + numbers
+
+        # if name is not being used by another robot
+        if  name not in self.names:
+            self.names += [name]
+            self.name = name
+        else:
+            self.reset()
