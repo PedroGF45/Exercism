40791348ffdd0546fa41f9beec8b07521f1a97e6
Pedro Brito <pedrobfh@gmail.com>
2023-03-16 16:30:45 +0000

    Added exercise 60 template

---
+++ python/twelve-days/twelve_days.py
@@ -0,0 +1,19 @@
+def recite(start_verse, end_verse):
+    
+    beginning_verse = "On the twelfth day of Christmas my true love gave to me: "
+    verses = [
+        "a Partridge in a Pear Tree",
+        "two Turtle Doves",
+        "three French Hens",
+        "four Calling Birds",
+        "five Gold Rings",
+        "six Geese-a-Laying",
+        "seven Swans-a-Swimming",
+        "eight Maids-a-Milking",
+        "nine Ladies Dancing",
+        "ten Lords-a-Leaping",
+        "eleven Pipers Piping",
+        "twelve Drummers Drumming"
+    ]
+
+
