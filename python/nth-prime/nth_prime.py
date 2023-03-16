aac8de70c1f801d1314c4a64ba435cf6bc9804ab
Pedro Brito <pedrobfh@gmail.com>
2023-03-16 16:20:03 +0000

    Solved exercise 59

--- python/nth-prime/nth_prime.py
+++ python/nth-prime/nth_prime.py
@@ -1,2 +1,20 @@
 def prime(number):
-    pass
+    
+    # there's no negative or 0th prime numbers
+    if number < 1:
+        raise ValueError('there is no zeroth prime')
+    
+    prime_numbers = [2]
+    i = 3
+    while len(prime_numbers) < number:
+
+        # if number is not divisible by any of the antecessors in prime_numbers list than it's a prime number
+        if all(map(lambda x: i % x != 0, prime_numbers)):
+            prime_numbers += [i]
+            i += 1
+        else:
+            i += 1
+    
+    return prime_numbers[number - 1]
+
+
