68c9085b2a87cd3b766d409d97a1ffeea7ae9bbd
Pedro Brito <pedrobfh@gmail.com>
2023-03-19 16:53:40 +0000

    Added and solved exercise 61

--- python/series/series.py
+++ python/series/series.py
@@ -1,2 +1,13 @@
 def slices(series, length):
-    pass
+    
+    #unsupported values
+    if series == '':
+        raise ValueError("series cannot be empty")
+    elif length > len(series):
+        raise ValueError("slice length cannot be greater than series length")
+    elif length == 0:
+        raise ValueError("slice length cannot be zero")
+    elif length < 0:
+        raise ValueError("slice length cannot be negative")
+    
+    return [series[i:i+length] for i in range(0, len(series) - length + 1)]
