80b06f57d2c102d75f4bd25df985720faedef508
Pedro Brito <pedrobfh@gmail.com>
2023-03-24 22:27:31 +0000

    Solved decode function

--- python/run-length-encoding/run_length_encoding.py
+++ python/run-length-encoding/run_length_encoding.py
@@ -1,6 +1,12 @@
 def decode(string):
-    pass
-
+    return ''.join((int(string[i])-1) * string[i + 1] if string[i].isdigit() else string[i] for i in range(0, len(string), 1))
 
 def encode(string):
-    pass
+    
+    for char in string:
+        ans = ''
+        if char == ans[-1]:
+            count = 1
+            ans += char
+        else:
+            ans += char
