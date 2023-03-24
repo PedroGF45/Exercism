ead7f8b0b4f003b1c4d721d550aa4f0de7d93f56
Pedro Brito <pedrobfh@gmail.com>
2023-03-24 22:49:56 +0000

    Solved encode function

--- python/run-length-encoding/run_length_encoding.py
+++ python/run-length-encoding/run_length_encoding.py
@@ -1,12 +1,8 @@
+from itertools import groupby
+
 def decode(string):
     return ''.join((int(string[i])-1) * string[i + 1] if string[i].isdigit() else string[i] for i in range(0, len(string), 1))
 
 def encode(string):
-    
-    for char in string:
-        ans = ''
-        if char == ans[-1]:
-            count = 1
-            ans += char
-        else:
-            ans += char
+    str_split = ["".join(g) for k, g in groupby(string)] 
+    return ''.join(f'{len(part)}{part[0]}' if len(part) > 1 else part[0] for part in str_split)
