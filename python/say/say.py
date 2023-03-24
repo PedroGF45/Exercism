d92639c36631322a79716e9e05546e8e30d0c147
Pedro Brito <pedrobfh@gmail.com>
2023-03-24 17:29:15 +0000

    Solved zero bugs

--- python/say/say.py
+++ python/say/say.py
@@ -3,8 +3,7 @@ def say(number):
     if number < 0 or number > 999999999999:
         raise ValueError("input out of range")
     
-    numbers_english = ['zero',
-                        'one',
+    numbers_english = ['one',
                         'two', 
                         'three', 
                         'four', 
@@ -33,31 +32,41 @@ def say(number):
         break_number = [str(number)[:(len(str(number)) % 3)]] + [str(number)[i:i+3] for i in range(len(str(number)) % 3, len(str(number)), 3)]
 
     def check_dozens(number):
+        if (len(ans) != 0 and number == 0) or number == 0:
+            return ''
         if number < 13 or number == 20:
-            return f'{numbers_english[number]}'
+            return f'{numbers_english[number-1]}'
         elif number > 12 and number < 20:
-            return f'{numbers_english[number]}' + 'een'
+            return f'{numbers_english[number-1]}' + 'een'
         elif number > 20 and str(number)[-1] == '0':
-            index = int(f'1{str(number)[0]}')
+            index = int(f'1{str(number-1)[0]}')
             return f'{numbers_english[index]}' + 'y'
         else:
-            index_dozens = int(f'1{str(number)[0]}')
-            index_digit = int(f'{str(number)[1]}')
-            if index_dozens == 12:
+            index_dozens = int(f'1{str(number)[0]}')-1
+            index_digit = int(f'{str(number)[1]}')-1
+            if index_dozens == 11:
                 return 'twenty-' + f'{numbers_english[index_digit]}'
-            if index_dozens == 14:
+            if index_dozens == 13:
                 return 'forty-' + f'{numbers_english[index_digit]}'
             return f'{numbers_english[index_dozens]}' + 'y-' + f'{numbers_english[index_digit]}'
     
     def check_hundred(number):
         if len(str(number)) == 3:
-            return f'{numbers_english[int(str(number)[0])]} hundred {check_dozens(int(str(number)[1:]))}' 
+            return f'{numbers_english[int(str(number)[0])-1]} hundred {check_dozens(int(str(number)[1:]))}' 
         else:
             return check_dozens(number) 
 
+    def check_grandeza(i):
+        if i < len(break_number) and int(break_number[i]) == 0 and i != 1:
+            return ''
+        return grandezas[len(break_number) - i]
+    
+    if len(break_number) == 1 and int(break_number[0]) == 0:
+        return 'zero'
+    
     ans = ''
-    for i in range(1, len(break_number)  + 1, 1):
-        print(i)
-        ans += check_hundred(int(break_number[i - 1])) + grandezas[len(break_number) - i]
+    for i in range(1, len(break_number) + 1, 1):
+        print(break_number)
+        ans += f'{check_hundred(int(break_number[i - 1]))}{check_grandeza(i)}'
 
-    return ans
+    return ans.strip()
