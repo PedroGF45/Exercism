d57d0d6b619bc43f675cce9c7d88072a6929021b
Pedro Brito <pedrobfh@gmail.com>
2023-03-25 00:01:42 +0000

    Solved exercise 67

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -1,6 +1,27 @@
 class Luhn:
     def __init__(self, card_num):
-        pass
+        self.number = card_num.replace(' ', '')
+
+    def double_it(self, number):
+        return ((int(number) * 2) if (int(number) * 2 < 10) else ((int(number) * 2) - 9))
 
     def valid(self):
-        pass
+
+        # not valid if has only one digit or if chars are not numbers
+        if len(self.number) == 1 or not self.number.isdigit(): 
+            return False
+
+        doubled = ''
+
+        if len(self.number) % 2 == 0:
+            parity = True
+        else:
+            parity = False
+
+        for i in range(0, len(self.number), 1):
+            if (parity and i % 2 == 0) or (not parity and i % 2 != 0):
+                doubled += str(self.double_it(self.number[i]))
+            else:
+                doubled += self.number[i]
+
+        return sum(map(lambda x: int(x), doubled)) % 10 == 0
