d4fcbd91c9bfe75f9fa7d081c9f33b852482a952
Pedro Brito <pedrobfh@gmail.com>
2023-06-01 14:01:36 +0100

    Solved exercise 71

--- python/prime-factors/prime_factors.py
+++ python/prime-factors/prime_factors.py
@@ -1,2 +1,15 @@
 def factors(value):
-    pass
+    
+    if value == 1:
+        return []
+    
+    res = []
+    prime = 2
+
+    while value > 1:
+        while (value % prime == 0):
+            value = int(value / prime)
+            res += [prime]
+        prime += 1
+
+    return res
