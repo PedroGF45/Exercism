721f873ba4374b539ce31d36e13b4e851797467a
Pedro Brito <pedrobfh@gmail.com>
2023-06-01 14:32:59 +0100

    solved exercise 72

--- python/dnd-character/dnd_character.py
+++ python/dnd-character/dnd_character.py
@@ -1,3 +1,36 @@
+from random import randint
+from math import floor
+
+def modifier(constitution):
+        return floor((constitution - 10)/2) 
+
 class Character:
+
     def __init__(self):
-        pass
+        # stats
+        self.strength = self.ability()
+        self.dexterity = self.ability()
+        self.intelligence = self.ability()
+        self.wisdom = self.ability()
+        self.charisma = self.ability()
+        self.constitution = self.ability()
+
+        # hitpoints
+        self.hitpoints = 10 + modifier(self.constitution)
+
+
+    def ability(self):
+
+        rolls = []
+
+        # get random dices' values
+        for i in range(4):
+            rolls += [randint(0, 6)]
+        
+        # sort the array for ascending order
+        rolls.sort()
+
+        # delete first element
+        rolls = rolls[1:]
+
+        return sum(rolls)
