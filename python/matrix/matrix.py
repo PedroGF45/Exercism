c181d4c96f0b6e474bc687da1144abc88a665082
Pedro Brito <pedrobfh@gmail.com>
2023-06-04 16:01:54 +0100

    Solved exercise 75

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,9 +1,21 @@
 class Matrix:
     def __init__(self, matrix_string):
-        pass
+        self.matrix = matrix_string.split('\n')
+        self.matrix = list(map(lambda x: x.split(), self.matrix))
 
     def row(self, index):
-        pass
+        row = list(self.matrix[index - 1])
+
+        row = list(map(lambda x: int(x), row))
+
+        return row
 
     def column(self, index):
-        pass
+        column = []
+        for row in self.matrix:
+            column += [row[index-1]]
+
+        column = list(map(lambda x: int(x), column))
+
+        return column
+    
