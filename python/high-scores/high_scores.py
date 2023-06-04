cbb58a04fcf48dbfd0f7c98de43cd22d0dfb1a81
Pedro Brito <pedrobfh@gmail.com>
2023-06-04 18:11:21 +0100

    Solved exercise 77

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,3 +1,15 @@
 class HighScores:
     def __init__(self, scores):
-        pass
+        self.scores = scores
+
+    def latest(self):
+        return self.scores[-1]
+    
+    def personal_best(self):
+        return max(self.scores)
+    
+    def personal_top_three(self):
+        sorted_list = self.scores.copy()
+        sorted_list.sort(reverse=True)
+        return sorted_list[:3]
+            
