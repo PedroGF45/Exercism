e9909f16a95b59b7d494ab0f7c77074efdc11644
Pedro Brito <pedrobfh@gmail.com>
2023-06-04 17:53:48 +0100

    Solved exercise 76

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,11 +1,42 @@
 class Allergies:
 
+    all = ['eggs',
+            'peanuts',
+            'shellfish',
+            'strawberries',
+            'tomatoes',
+            'chocolate',
+            'pollen',
+            'cats']
+
     def __init__(self, score):
-        pass
+        while score > 256:
+            score -= 256
+        self.score = bin(score)[2:]
 
     def allergic_to(self, item):
-        pass
+        index = len(self.score) - 1
+
+        for digit in self.score:
+
+            if (digit == '1' and Allergies.all[index] == item):
+                return True
+            
+            index -= 1
+        
+        return False
 
     @property
     def lst(self):
-        pass
+        res = []
+        index = len(self.score) - 1
+
+        for digit in self.score:
+
+            if (digit == '1'):
+                res += [Allergies.all[index]]
+            
+            index -= 1
+        
+        return res
+ 
