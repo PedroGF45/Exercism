da2f61d2874a057a720bdd8d27ee5178d370fe59
Pedro Brito <pedrobfh@gmail.com>
2024-08-08 21:54:57 +0100

    Completed poker exercise with community copy

--- python/poker/poker.py
+++ python/poker/poker.py
@@ -1,58 +1,51 @@
-STRAIGHT_FLUSH = 8
-FOUR_OF_A_KIND = 7 
-FULL_HOUSE = 6
-FLUSH = 5
-STRAIGHT = 4
-THREE_OF_A_KIND = 3
-TWO_PAIR = 2
-ONE_PAIR = 1
-HIGH_CARD = 0
 
 def best_hands(hands):
-    
+    hands = serialize(hands)
+    return [
+        deserialize(hand)
+        for hand in hands
+        if card_ranks(hand) == card_ranks(max(hands, key=hand_rank))
+    ]
+
+
+def serialize(hands):
+    """["10C AH"] -> [("0", "C"), ("A", "H"),]"""
+    _hands = []
     for hand in hands:
-        print(check_hands(hand.replace('K', '13').replace('Q', '12').replace('J', '11').replace('A', '14').split()))
+        _hand = []
+        for card in hand.split():
+            _hand.append((card[-2], card[-1]))
+        _hands.append(_hand)
+    return _hands
 
 
-def check_hands(hand):
+def deserialize(hand):
+    """[("0", "C"), ("A", "H"),] -> ["10C AH"]"""
+    return " ".join(["".join(["10" if r == "0" else r, s]) for r, s in hand])
 
-    print(hand)
-    # straight flush
-    naipe_sf = all(map(lambda x: hand[x][-1] == hand[x+1][-1] , range(1, len(hand)-1, 1)))
-    numbers_sf = all(map(lambda x: (int(hand[x+1][:-1]) - int(hand[x][:-1]) == 1) , range(0, len(hand)-1, 1)))
 
-    if naipe_sf and numbers_sf:
-        return STRAIGHT_FLUSH
-    
+def hand_rank(hand):
+    """takes serialized hands and resturns its ranking"""
+    ranks = card_ranks(hand)
+    groups = [(ranks.count(i), i) for i in set(ranks)]
+    groups.sort(reverse=True)
+    counts, number = zip(*groups)
+    straight = (len(counts) == 5) and (max(number) - min(number) == 4)
+    flush = len(set([s for r, s in hand])) == 1
+    return (
+        8 if straight and flush else
+        7 if counts == (4, 1) else
+        6 if counts == (3, 2) else
+        5 if flush else
+        4 if straight else
+        3 if counts == (3, 1, 1) else
+        2 if counts == (2, 2, 1) else
+        1 if counts == (2, 1, 1, 1) else
+        0, number
+    )
 
-    # four of a kind
-    if (hand.count(hand[0]) == 4 or hand.count(hand[2]) == 4):
-        return FOUR_OF_A_KIND
-    
-    # full house
-    numbers_fh = sorted(hand)[:5]
-    if ((numbers_fh.count(numbers_fh[0]) == 3 and numbers_fh.count(numbers_fh[3]) == 2) or 
-        (numbers_fh.count(numbers_fh[0]) == 2 and numbers_fh.count(numbers_fh[2]) == 3)):
-        return FULL_HOUSE
-    
-    # flush
-    if naipe_sf:
-        return FLUSH
-    
-    # straight
-    if numbers_sf:
-        return STRAIGHT
-    
-    # three of a kind
-    if ((numbers_fh.count(numbers_fh[0]) == 3) or numbers_fh.count(numbers_fh[2]) == 3):
-        return THREE_OF_A_KIND
-    
-    # two pair
-    if (numbers_fh.count(numbers_fh[1]) == 2 and numbers_fh.count(numbers_fh[3]) == 2):
-        return TWO_PAIR
-    
-    # one pair
-    if (numbers_fh.count(numbers_fh[1]) == 2 or numbers_fh.count(numbers_fh[3]) == 2):
-        return TWO_PAIR
-    
-    return HIGH_CARD
+
+def card_ranks(hand):
+    ranks = ["--234567890JQKA".index(r) for r, s in hand]
+    ranks.sort(reverse=True)
+    return [5, 4, 3, 2, 1] if (ranks == [14, 5, 4, 3, 2]) else ranks
