cbab10b1f75a80ba50330b711bfa0cbc09d64e6c
Pedro Brito <pedrobfh@gmail.com>
2025-06-20 21:24:27 +0100

    :sparkles: feat: solve markdown

--- python/markdown/markdown.py
+++ python/markdown/markdown.py
@@ -1,77 +1,44 @@
 import re
 
 
+def _parse_emphasis(text):
+    """Parses bold (__) and italic (_) markup."""
+    text = re.sub(r"__(.*?)__", r"<strong>\1</strong>", text)
+    text = re.sub(r"_(.*?)_", r"<em>\1</em>", text)
+    return text
+
+
 def parse(markdown):
-    lines = markdown.split('\n')
-    res = ''
+    lines = markdown.split("\n")
+    html = ""
     in_list = False
-    in_list_append = False
-    for i in lines:
-        if re.match('###### (.*)', i) is not None:
-            i = '<h6>' + i[7:] + '</h6>'
-        elif re.match('##### (.*)', i) is not None:
-            i = '<h5>' + i[6:] + '</h5>'
-        elif re.match('#### (.*)', i) is not None:
-            i = '<h4>' + i[5:] + '</h4>'
-        elif re.match('### (.*)', i) is not None:
-            i = '<h3>' + i[4:] + '</h3>'
-        elif re.match('## (.*)', i) is not None:
-            i = '<h2>' + i[3:] + '</h2>'
-        elif re.match('# (.*)', i) is not None:
-            i = '<h1>' + i[2:] + '</h1>'
-        m = re.match(r'\* (.*)', i)
-        if m:
+
+    for line in lines:
+        # Headers
+        header_match = re.match(r"(#+) (.*)", line)
+        if header_match and len(header_match.group(1)) < 7:
+            if in_list:
+                html += "</ul>"
+                in_list = False
+            level = len(header_match.group(1))
+            content = _parse_emphasis(header_match.group(2))
+            html += f"<h{level}>{content}</h{level}>"
+        # List items
+        elif line.startswith("* "):
             if not in_list:
+                html += "<ul>"
                 in_list = True
-                is_bold = False
-                is_italic = False
-                curr = m.group(1)
-                m1 = re.match('(.*)__(.*)__(.*)', curr)
-                if m1:
-                    curr = m1.group(1) + '<strong>' + \
-                        m1.group(2) + '</strong>' + m1.group(3)
-                    is_bold = True
-                m1 = re.match('(.*)_(.*)_(.*)', curr)
-                if m1:
-                    curr = m1.group(1) + '<em>' + m1.group(2) + \
-                        '</em>' + m1.group(3)
-                    is_italic = True
-                i = '<ul><li>' + curr + '</li>'
-            else:
-                is_bold = False
-                is_italic = False
-                curr = m.group(1)
-                m1 = re.match('(.*)__(.*)__(.*)', curr)
-                if m1:
-                    is_bold = True
-                m1 = re.match('(.*)_(.*)_(.*)', curr)
-                if m1:
-                    is_italic = True
-                if is_bold:
-                    curr = m1.group(1) + '<strong>' + \
-                        m1.group(2) + '</strong>' + m1.group(3)
-                if is_italic:
-                    curr = m1.group(1) + '<em>' + m1.group(2) + \
-                        '</em>' + m1.group(3)
-                i = '<li>' + curr + '</li>'
+            content = _parse_emphasis(line[2:])
+            html += f"<li>{content}</li>"
+        # Paragraphs
         else:
             if in_list:
-                in_list_append = True
+                html += "</ul>"
                 in_list = False
+            content = _parse_emphasis(line)
+            html += f"<p>{content}</p>"
 
-        m = re.match('<h|<ul|<p|<li', i)
-        if not m:
-            i = '<p>' + i + '</p>'
-        m = re.match('(.*)__(.*)__(.*)', i)
-        if m:
-            i = m.group(1) + '<strong>' + m.group(2) + '</strong>' + m.group(3)
-        m = re.match('(.*)_(.*)_(.*)', i)
-        if m:
-            i = m.group(1) + '<em>' + m.group(2) + '</em>' + m.group(3)
-        if in_list_append:
-            i = '</ul>' + i
-            in_list_append = False
-        res += i
     if in_list:
-        res += '</ul>'
-    return res
+        html += "</ul>"
+
+    return html
